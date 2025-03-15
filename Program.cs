aaf00f529a4a27c6c5a299966399a9a943d7f737
Miguel <mdias1300@gmail.com>
2025-03-14 22:42:19 -0300

    fazendo

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+String nomeDigitado;
+String nomeLeet; 
+
+Console.WriteLine("nome...:");
+nomeDigitado = Console.ReadLine()!;
+
+nomeLeet = nomeDigitado.Replace("A","4")
+                       .Replace("E","3");
+
+Console.WriteLine($"bom dia, {nomeDigitado}!");
